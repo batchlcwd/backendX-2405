d299022502e43d90a0437631313c8e1d20885ecf
batchlcwd <batchlcwd@gmail.com>
2025-06-05 22:41:59 +0530

    05:06:2025 smart result completed

--- mvc/smart-result/src/main/java/com/substring/smartresult/controllers/ResultController.java
+++ mvc/smart-result/src/main/java/com/substring/smartresult/controllers/ResultController.java
@@ -1,15 +1,15 @@
 package com.substring.smartresult.controllers;
 
+import com.substring.smartresult.entities.Mark;
 import com.substring.smartresult.entities.Student;
 import com.substring.smartresult.payload.StudentForm;
 import com.substring.smartresult.service.ResultService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.DecimalFormat;
 
 @Controller
 @RequestMapping("/results")
@@ -76,6 +76,61 @@ public class ResultController {
     }
 
 
+//    view result controller method
+
+    //@RequestMapping(value = "/view-result", method = RequestMethod.POST)
+    @PostMapping("/view-result")
+    public String viewResultByRollNumber(
+            @RequestParam("rollNumber") String rollNumber, Model model) {
+        System.out.println("Roll Number: " + rollNumber);
+
+        Student student = resultService.getResultByRollNumber(rollNumber);
+//        System.out.println(student.getName());
+//        System.out.println(student.getMarkList().size());
+        if (student == null) {
+            return "result_not_found"; // Placeholder for result not found
+        } else {
+
+            double totalMarks = 0;
+            double totalMaxMarks = 0;
+
+            for (Mark mark : student.getMarkList()) {
+                totalMarks += mark.getMarks();
+                totalMaxMarks += mark.getMaxMarks();
+            }
+
+            double percentage = (totalMarks / totalMaxMarks) * 100;
+//            model.addAttribute("percentage", String.format("%.2f",percentage)+"");
+
+            DecimalFormat df = new DecimalFormat("#.00");
+            model.addAttribute("percentage", df.format(percentage));
+            model.addAttribute("totalMarks", Double.valueOf(totalMarks).intValue());
+            model.addAttribute("totalMaxMarks", Double.valueOf(totalMaxMarks).intValue());
+
+            String totalGrade = "F";
+
+            if (percentage > 90) {
+                totalGrade = "A+";
+            } else if (percentage > 80 && percentage <= 90) {
+                totalGrade = "A";
+            } else if (percentage > 70 && percentage <= 80) {
+                totalGrade = "B+";
+            } else if (percentage > 60 && percentage <= 70) {
+                totalGrade = "B";
+            } else {
+                totalGrade = "F";
+            }
+
+            model.addAttribute("totalGrade", totalGrade);
+
+
+            model.addAttribute("student", student);
+            model.addAttribute("marks", student.getMarkList());
+            return "result_data";
+        }
+
+
+    }
 
 
 }
